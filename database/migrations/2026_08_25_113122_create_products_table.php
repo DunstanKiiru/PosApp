6c6fcb8d4e0db933aeb0b0c33b8b7a9607e44c30
DunstanKiiru <alexkiiru7@gmail.com>
2026-08-25 14:32:44 +0300

    adds products table

--- database/migrations/2026_08_25_113122_create_products_table.php
+++ database/migrations/2026_08_25_113122_create_products_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('name');
-            $table->text('description')->nullable();//can be empty
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
     }
 };
